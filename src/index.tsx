93012d80d39e6463e93fb100770d5c688a40ff25
Bogdan Udovenko <bog.udov@gmail.com>
2023-07-01 20:38:16 +0300

    install typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
 );
