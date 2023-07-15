464341c5f24cf006f987a282fd05dd7d54802d3c
Bogdan Udovenko <bog.udov@gmail.com>
2023-07-15 20:39:26 +0300

    added api for fetching cities

--- src/components/search/search.component.tsx
+++ src/components/search/search.component.tsx
@@ -1,48 +1,54 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, useMemo, useState } from 'react'
 
 import { FormControl } from 'react-bootstrap'
 
+import debounce from 'lodash.debounce'
+
+import { getCityByName } from '~/services/GeoDb/city'
 import { CityModel } from '~/types/app.models'
-import { PUBLIC_URL } from '~/utils/constants/app.constants'
 
 import CityList from '../city-list/city-list.component'
 
 import './search.style.scss'
 
 const Search = () => {
-  const [cities, setCities] = useState<CityModel[]>([])
   const [cityList, setCityList] = useState<CityModel[]>([])
 
-  useEffect(() => {
-    fetch(`${PUBLIC_URL}/cities.json`)
-      .then((cities) => cities.json())
-      .then((json) => setCities(json))
-  }, [])
+  const handleCity = useMemo(
+    () =>
+      debounce(async (event: ChangeEvent<HTMLInputElement>) => {
+        try {
+          const enteredCity = event.target.value
 
-  const handleCity = (event: ChangeEvent<HTMLInputElement>) => {
-    const enteredCity = event.target.value
+          if (enteredCity !== null && enteredCity.length >= 3) {
+            const response = await getCityByName(enteredCity)
 
-    if (enteredCity !== null && enteredCity.length >= 3) {
-      const filterCities = cities.filter((city) =>
-        city.name.toLowerCase().includes(enteredCity.toLowerCase())
-      )
-      setCityList(filterCities)
-    } else {
-      if (cityList.length) setCityList([])
-    }
-  }
+            const cities = response?.data?.data
+
+            setCityList(cities)
+          } else {
+            setCityList([])
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      }, 1000),
+    []
+  )
+
+  const hasCities = !!cityList.length
 
   return (
     <div className="search_block">
       <h4>Search city</h4>
+
       <FormControl
         type="input"
         onInput={handleCity}
         placeholder="Enter 3 and more letters"
       />
-      {cityList.length ? (
-        <CityList cities={cityList} clearCityList={setCityList} />
-      ) : null}
+
+      {hasCities && <CityList cities={cityList} clearCityList={setCityList} />}
     </div>
   )
 }
--- src/utils/constants/api.constants.ts
+++ src/utils/constants/api.constants.ts
@@ -1,1 +1,3 @@
 export const WEATHER_API_KEY = process?.env?.REACT_APP_WEATHER_API_KEY || ''
+
+export const GEO_API_KEY = process?.env?.REACT_APP_GEO_API_KEY || ''
